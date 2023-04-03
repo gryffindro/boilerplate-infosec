39823235806462c70c8505df9bfdcabf32f05d90
Gavyn Alejandro <gavyn.alejandro@outlook.com>
2023-04-03 11:06:33 -0400

    Completion of Lesson 2-4 & Updating Packages

--- myApp.js
+++ myApp.js
@@ -11,7 +11,10 @@ app.use('/_api', api);
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
-let port = process.env.PORT || 3000;
+helmet.hidePoweredBy()
+helmet.frameguard({action:'deny'})
+helmet.xssFilter()
+let port = process.env.PORT || 3000;  
 app.listen(port, () => {
   console.log(`Your app is listening on port ${port}`);
 });
